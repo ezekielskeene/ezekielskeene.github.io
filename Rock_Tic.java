62fdebe849b68707d55c60afecd39f242b5ec5f4
ezekielskeene <113312161+ezekielskeene@users.noreply.github.com>
2024-05-14 17:14:26 -0400

    Bug fixes and bad computer to play against

--- Rock_Tic.java
+++ Rock_Tic.java
@@ -3,24 +3,24 @@ import java.util.*;
 
 public class Rock_Tic {
     public static void setBoard() {
-
+        ArrayList<String> blueSpaces = new ArrayList<>(spaces);
         for (int i = 0; i < 9; i++) {
             if (amount.get(i).equals("3")) {
-                spaces.set(i, "\u001B[34m" + spaces.get(i) + "\u001B[0m");
+                blueSpaces.set(i, "\u001B[34m" + blueSpaces.get(i) + "\u001B[0m");
             }
         }
 
         useful.clearScreen();
         useful.print("       |       |       ");
-        useful.print("   " +  spaces.get(0) + "   |   " + spaces.get(1) + "   |   " + spaces.get(2) + "   ");
+        useful.print("   " +  blueSpaces.get(0) + "   |   " + blueSpaces.get(1) + "   |   " + blueSpaces.get(2) + "   ");
         useful.print("      " + amount.get(0) + "|      " + amount.get(1) + "|      " + amount.get(2));
         useful.print("-----------------------");
         useful.print("       |       |       ");
-        useful.print("   " +  spaces.get(3) + "   |   " + spaces.get(4) + "   |   " + spaces.get(5) + "   ");
+        useful.print("   " +  blueSpaces.get(3) + "   |   " + blueSpaces.get(4) + "   |   " + blueSpaces.get(5) + "   ");
         useful.print("      " + amount.get(3) + "|      " + amount.get(4) + "|      " + amount.get(5));
         useful.print("-----------------------");
         useful.print("       |       |       ");
-        useful.print("   " +  spaces.get(6) + "   |   " + spaces.get(7) + "   |   " + spaces.get(8) + "   ");
+        useful.print("   " +  blueSpaces.get(6) + "   |   " + blueSpaces.get(7) + "   |   " + blueSpaces.get(8) + "   ");
         useful.print("      " + amount.get(6) + "|      " + amount.get(7) + "|      " + amount.get(8));
         
     }
@@ -51,48 +51,85 @@ public class Rock_Tic {
         firstRound = true;
     }
 
-    public static void turn(String player) {
+    public static void turn(String player) throws InterruptedException{
         System.out.print(player + "'s Move: ");
         boolean valid = false;
-        int userMove = -1;
-        while (!valid) {
-            String userMoveString = test.nextLine();
-            try {
-                userMove = Integer.valueOf(userMoveString);
-            }
-            catch (Exception e){
-
-            }
-            if (player == "Rock") {
-                for (int i = 0; i < rockSpaces.size(); i++) {
-                    if (rockSpaces.get(i) == null) {
-
-                    } else if (userMove == rockSpaces.get(i)) {
-                        valid = true;
-                    }
-                }
-            } else if (player == "Paper") {
-                for (int i = 0; i < paperSpaces.size(); i++) {
-                    if (paperSpaces.get(i) == null) {
-
-                    } else if (userMove == paperSpaces.get(i)) {
-                        valid = true;
-                    }
-                }
-            } else {
-                for (int i = 0; i < scissorsSpaces.size(); i++) {
-                    if (scissorsSpaces.get(i) == null) {
-
-                    } else if (userMove == scissorsSpaces.get(i)) {
-                        valid = true;
-                    }
+        Integer userMove = -1;
+        boolean possibleMove = false;
+        if (player == "Rock") {
+            for (int i = 0 ; i < 9; i++) {
+                if (!(rockSpaces.get(i) == -1)) {
+                    possibleMove = true;
                 }
             }
-            if (!valid) {
-                useful.print("Not a valid space. Try again: ");
+        } else if (player == "Paper") {
+            for (int i = 0 ; i < 9; i++) {
+                if (!(paperSpaces.get(i) == -1)) {
+                    possibleMove = true;
+                }
+            }
+        } else {
+            for (int i = 0 ; i < 9; i++) {
+                if (!(scissorsSpaces.get(i) == -1)) {
+                    possibleMove = true;
+                }
             }
         }
-        move((userMove-1), player.substring(0, 1));
+        if (player.equals(Computer) && possibleMove) {
+            while (userMove == 0 || userMove == -1) {
+                if (player == "Rock") {
+                    userMove = rockSpaces.get(rand.nextInt(9));
+                } else if (player == "Paper") {
+                    userMove = paperSpaces.get(rand.nextInt(9));
+                } else {
+                    userMove = scissorsSpaces.get(rand.nextInt(9));
+                }
+            }
+            Thread.sleep(1000);
+        } else if (possibleMove){
+            while (!valid) {
+                String userMoveString = test.nextLine();
+                try {
+                    userMove = Integer.valueOf(userMoveString);
+                }
+                catch (Exception e){
+
+                }
+                if (player == "Rock") {
+                    for (int i = 0; i < rockSpaces.size(); i++) {
+                        if (rockSpaces.get(i) == -1) {
+
+                        } else if (userMove == rockSpaces.get(i)) {
+                            valid = true;
+                        }
+                    }
+                } else if (player == "Paper") {
+                    for (int i = 0; i < paperSpaces.size(); i++) {
+                        if (paperSpaces.get(i) == -1) {
+
+                        } else if (userMove == paperSpaces.get(i)) {
+                            valid = true;
+                        }
+                    }
+                } else {
+                    for (int i = 0; i < scissorsSpaces.size(); i++) {
+                        if (scissorsSpaces.get(i) == -1) {
+
+                        } else if (userMove == scissorsSpaces.get(i)) {
+                            valid = true;
+                        }
+                    }
+                }
+                if (!valid) {
+                    useful.print("Not a valid space. Try again: ");
+                }
+            }
+        } else {
+            useful.print("No possible moves.");
+        }
+        if (possibleMove) {
+            move((userMove-1), player.substring(0, 1));
+        }
         setBoard();
     }
 
@@ -101,29 +138,29 @@ public class Rock_Tic {
         String currentAmount = amount.get(space);
         String setAmount = "0";
         if (type.equals("R")) {
-            rockSpaces.set(space, null);
-            scissorsSpaces.set(space, null);
+            rockSpaces.set(space, -1);
+            scissorsSpaces.set(space, -1);
             if (firstRound) {
-                paperSpaces.set(space, null);
+                paperSpaces.set(space, -1);
             } else {
                 paperSpaces.set(space, space+1);
             }
         } else if (type.equals("P")) {
-            rockSpaces.set(space, null);
+            rockSpaces.set(space, -1);
             if (firstRound) {
-                scissorsSpaces.set(space, null);
+                scissorsSpaces.set(space, -1);
             } else {
                 scissorsSpaces.set(space, space+1);
             }
-            paperSpaces.set(space, null);
+            paperSpaces.set(space, -1);
         } else if (type.equals("S")) {
             if (firstRound) {
-                rockSpaces.set(space, null);
+                rockSpaces.set(space, -1);
             } else {
                 rockSpaces.set(space, space+1);
             }
-            scissorsSpaces.set(space, null);
-            paperSpaces.set(space, null);
+            scissorsSpaces.set(space, -1);
+            paperSpaces.set(space, -1);
         }
         if (currentAmount == "0") {
             setAmount = "1";
@@ -131,9 +168,9 @@ public class Rock_Tic {
             setAmount = "2";
         } else{
             setAmount = "3";
-            rockSpaces.set(space, null);
-            paperSpaces.set(space, null);
-            scissorsSpaces.set(space, null);
+            rockSpaces.set(space, -1);
+            paperSpaces.set(space, -1);
+            scissorsSpaces.set(space, -1);
         }
 
         
@@ -282,10 +319,17 @@ public class Rock_Tic {
             useful.print(player + " Wins!");
             return true;
         }
-        return false;
+
+        for (int i = 0; i < 9; i++) {
+            if (!(rockSpaces.get(i) == -1 && paperSpaces.get(i) == -1 && scissorsSpaces.get(i) == -1)) {
+                return false;
+            }
+        }
+        useful.print("No one wins :(");
+        return true;
     }
 
-    public static void game() {
+    public static void game() throws InterruptedException{
         reset();
         win = false;
         String player = "Rock";
@@ -325,8 +369,30 @@ public class Rock_Tic {
     public static Scanner test = new Scanner(System.in);
     public static boolean play = true;
     public static boolean firstRound = true;
+    public static String Computer = null;
+    static Random rand = new Random();
     public static void main(String[] args) throws InterruptedException{
+        useful.clearScreen();
         while (play) {
+            useful.print("Play against computer (y or n)? ");
+            String useComputer = test.nextLine();
+            while (!(useComputer.equals("y") || useComputer.equals("n"))) {
+                useful.print("Not a valid answer. Try again: ");
+                useComputer = test.nextLine();
+            }
+            if (useComputer.equals("y")) {
+                boolean validComputer = false;
+                useful.print("Which one (Rock,Paper,or Scissors): ");
+                while (!validComputer) {
+                    Computer = test.nextLine();
+                    if (Computer.equals("Rock") || Computer.equals("Paper") || Computer.equals("Scissors")) {
+                        validComputer = true;
+                    } else {
+                        useful.print("Not a valid answer. Try again: ");
+                    }
+                }
+                
+            }
             game();
             useful.print("Play again:");
             String playInput = " ";
